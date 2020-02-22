07973493186695ee26f849ce2e29c846abd050d0
Dhabitchi <60819843+Dhabitchi@users.noreply.github.com>
2020-02-22 13:17:56 +0700

    fix typo nama tabel

--- app/Rekapjob.php
+++ app/Rekapjob.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rekapjob extends Model
 {
-    protected $table = 'Rekapjobs';
+    protected $table = 'rekapjobs';
     protected $primaryKey = 'r_id';
     protected $fillable = ['nama','job','waktu','waktu2','status'];
 }
